725db04d2b38d950d794810872cfa92c37592c7c
Monica <monipoo94@gmail.com>
2020-03-11 04:12:11 +0530

    Added other popups and in timeline

--- js/familytree.js
+++ js/familytree.js
@@ -1,6 +1,21 @@
 /* Open when someone clicks on the span element */
 function openNav(id) {
     console.log(id);
+    if (id == "poddar-monica" || id == "poddar-monica-time") {
+        document.getElementById("overlay-img").src = "resources/assignments/monica.png"
+    } else if (id == "poddar-kamal" || id == "poddar-kamal-time") {
+        document.getElementById("overlay-img").src = "resources/assignments/poddar-kamal.png"
+    } else if (id == "poddar-anjana" || id == "poddar-anjana-time") {
+        document.getElementById("overlay-img").src = "resources/assignments/poddar-anjana.png"
+    } else if (id == "das-ranjan" || id == "das-ranjan-time") {
+        document.getElementById("overlay-img").src = "resources/assignments/das-ranjan.png"
+    } else if (id == "das-swapna" || id == "das-swapna-time") {
+        document.getElementById("overlay-img").src = "resources/assignments/das-swapna.png"
+    } else if (id == "kaku" || id == "kaku-time") {
+        document.getElementById("overlay-img").src = "resources/assignments/kaku.png"
+    } else {
+        document.getElementById("overlay-img").src = "resources/assignments/monica.png"
+    }
     document.getElementById("myNav").style.width = "100%";
 }
 
