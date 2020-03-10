66b91624c5c7d94cf80cda5f3dca1684bee44da5
Monica <monipoo94@gmail.com>
2020-03-11 03:10:29 +0530

    Popup

--- js/familytree.js
+++ js/familytree.js
@@ -1,7 +1,7 @@
 /* Open when someone clicks on the span element */
 function openNav(id) {
     console.log(id);
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.width = "40%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
